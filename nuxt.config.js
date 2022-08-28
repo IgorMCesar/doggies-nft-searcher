a7937c121356be3b40c8924a6dc5e52c644c46f7
IgorMCesar <igormarracho01@gmail.com>
2022-08-28 19:33:00 +0100

    chore: adding favico and title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'the-sandbox-challenge',
+    title: 'The Doggies Explorer',
     htmlAttrs: {
       lang: 'en',
     },
